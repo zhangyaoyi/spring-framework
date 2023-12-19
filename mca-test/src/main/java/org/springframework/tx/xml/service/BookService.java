5dcdf8b0396ce24e56b25103d08e1928a387a4f5
yyzhang <zhangyaoyi2011@hotmail.com>
2023-12-19 22:09:05 +0800

    test

--- mca-test/src/main/java/org/springframework/tx/xml/service/BookService.java
+++ mca-test/src/main/java/org/springframework/tx/xml/service/BookService.java
@@ -14,11 +14,6 @@ public class BookService {
         this.bookDao = bookDao;
     }
 
-    /**
-     * 结账：传入哪个用户买了哪本书
-     * @param username
-     * @param id
-     */
     public void checkout(String username,int id){
 
         try {
@@ -26,9 +21,5 @@ public class BookService {
         } catch (Exception e) {
             e.printStackTrace();
         }
-//        for (int i = 1 ;i>=0 ;i--)
-//            System.out.println(10/i);
-//        int price = bookDao.getPrice(id);
-//        bookDao.updateBalance(username,price);
     }
 }
