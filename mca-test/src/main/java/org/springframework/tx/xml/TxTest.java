4670089e586f5420bc6a05718787f802038798ee
Yao Yi Zhang <yzhang252@dxc.com>
2024-01-02 23:40:22 +0800

    dev

--- mca-test/src/main/java/org/springframework/tx/xml/TxTest.java
+++ mca-test/src/main/java/org/springframework/tx/xml/TxTest.java
@@ -1,16 +1,13 @@
 package org.springframework.tx.xml;
 
-import org.springframework.tx.xml.service.BookService;
 import org.springframework.cglib.core.DebuggingClassWriter;
+import org.springframework.tx.xml.service.BookService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.sql.SQLException;
-
-// 把xml配置的方式准备对象的过程画一个流程图出来
 public class TxTest {
-    public static void main(String[] args) throws SQLException {
-        //System.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY,"d:\\code");
+    public static void main(String[] args) {
+		System.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY,"d:\\code");
         ApplicationContext context = new ClassPathXmlApplicationContext("tx.xml");
         BookService bookService = context.getBean("bookService", BookService.class);
         bookService.checkout("zhangsan",1);
